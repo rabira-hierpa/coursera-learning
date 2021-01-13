6af71ac6318ca8ec31cb0bd44ef32a6d77aca494
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-01-13 08:44:16 +0300

    Presentational and Container components

--- React/confusion/src/components/MenuComponent.js
+++ React/confusion/src/components/MenuComponent.js
@@ -11,12 +11,6 @@ import DishdetailComponent from './DishdetailComponent';
 export default class MenuComponent extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			selectedDish: null,
-		};
-	}
-	onDishSelect(dish) {
-		this.setState({ selectedDish: dish });
 	}
 
 	renderDish(dish) {
@@ -39,7 +33,7 @@ export default class MenuComponent extends Component {
 		const menu = this.props.dishes.map((dish) => {
 			return (
 				<div key={dish.id} className='col-12 col-md-5 m-1'>
-					<Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+					<Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
 						<CardImg width='100%' src={dish.image} alt={dish.name} />
 						<CardImgOverlay>
 							<CardTitle>{dish.name}</CardTitle>
@@ -51,11 +45,6 @@ export default class MenuComponent extends Component {
 		return (
 			<div className='container'>
 				<div className='row'>{menu}</div>
-				{this.state.selectedDish ? (
-					<DishdetailComponent dish={this.state.selectedDish} />
-				) : (
-					<div></div>
-				)}
 			</div>
 		);
 	}
