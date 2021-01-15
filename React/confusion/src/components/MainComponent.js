32eb167d21c50e16605e04c4a18e1ff545fb4778
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-01-15 15:16:50 +0300

    Single Page Applications Part 1

--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -1,43 +1,49 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
-import DishdetailComponent from './DishdetailComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			dishes: DISHES,
-			selectedDish: null,
+			comments: COMMENTS,
+			promotions: PROMOTIONS,
+			leaders: LEADERS,
 		};
-	}
-	onDishSelect(dishId) {
-		this.setState({ selectedDish: dishId });
 	}
 
 	render() {
+		const HomePage = () => {
+			return (
+				<Home
+					dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+					promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+					leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+				/>
+			);
+		};
 		return (
 			<div>
 				<Header />
-				<Menu
-					dishes={this.state.dishes}
-					onClick={(dishId) => this.onDishSelect(dishId)}
-				/>
-				{this.state.selectedDish != null ? (
-					<div className='container'>
-						<DishdetailComponent
-							dish={
-								this.state.dishes.filter(
-									(dish) => dish.id === this.state.selectedDish
-								)[0]
-							}
-						/>
-					</div>
-				) : (
-					<div></div>
-				)}
+				<Switch>
+					<Route path='/home' component={HomePage} />
+					<Route exact path='contactus' component={Contact} />
+					<Route
+						exact
+						path='/menu'
+						component={() => <Menu dishes={this.state.dishes} />}
+					/>
+					<Redirect to='/home' />
+				</Switch>
 				<Footer />
 			</div>
 		);
