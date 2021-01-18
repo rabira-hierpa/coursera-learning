17c5b343de038a0f72a7aa84bf64084ffcaadd8b
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-01-18 07:59:32 +0300

    rendered dish details with separte UI and comments

--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -9,7 +9,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
-
+import DishDetail from './DishdetailComponent';
 class Main extends Component {
 	constructor(props) {
 		super(props);
@@ -31,17 +31,33 @@ class Main extends Component {
 				/>
 			);
 		};
+
+		const DishWithId = ({ match }) => {
+			return (
+				<DishDetail
+					dish={
+						this.state.dishes.filter(
+							(dish) => dish.id === parseInt(match.params.dishId, 10)
+						)[0]
+					}
+					comments={this.state.comments.filter(
+						(comment) => comment.dishId === parseInt(match.params.dishId, 10)
+					)}
+				/>
+			);
+		};
 		return (
 			<div>
 				<Header />
 				<Switch>
 					<Route path='/home' component={HomePage} />
-					<Route exact path='contactus' component={Contact} />
+					<Route exact path='/contactus' component={Contact} />
 					<Route
 						exact
 						path='/menu'
 						component={() => <Menu dishes={this.state.dishes} />}
 					/>
+					<Route path='/menu/:dishId' component={DishWithId} />
 					<Redirect to='/home' />
 				</Switch>
 				<Footer />
