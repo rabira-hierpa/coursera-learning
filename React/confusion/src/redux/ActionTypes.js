1c1b00bdda19007bc7ad096ca3e6c549a69bca04
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-02-02 07:48:56 +0300

    added more action types

--- React/confusion/src/redux/ActionTypes.js
+++ React/confusion/src/redux/ActionTypes.js
@@ -1,2 +1,5 @@
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const DISHES_LOADING = 'DISHES_LOADING';
+export const DISHES_FAILDED = 'DISHES_FAILED';
+export const ADD_DISHES = 'ADD_DISHES';
