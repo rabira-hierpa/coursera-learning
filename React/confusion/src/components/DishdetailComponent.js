3898adbe7b6976ea845aac8403e2348cf7533043
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-02-02 07:49:18 +0300

    added a loading and error message component

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -18,7 +18,7 @@ import {
 import Moment from 'moment';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
-
+import Loading from './LoadingComponent';
 function RenderDish({ dish }) {
 	if (dish) {
 		return (
@@ -68,40 +68,57 @@ const RenderComments = ({ comments, addComment, dishId }) => {
 };
 
 const DishDetail = (props) => {
-	return (
-		<React.Fragment>
+	if (props.isLoading) {
+		return (
 			<div className='container'>
 				<div className='row'>
-					<Breadcrumb>
-						{/* <BreadcrumbItem>
-								<Link to='/home'>Home</Link>
-							</BreadcrumbItem> */}
-						<BreadcrumbItem>
-							<Link to='/menu'>Menu</Link>
-						</BreadcrumbItem>
-						<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-					</Breadcrumb>
-					<div className='col-12'>
-						<h3>{props.dish.name}</h3>
-						<hr />
-					</div>
-				</div>
-				<div className='row'>
-					<div className='col-lg-5 col-sm-12 m-1'>
-						<RenderDish dish={props.dish} />
-					</div>
-					<div className='col-lg-5 col-md-5 col-sm-12 m-1'>
-						<h4>Comments </h4>
-						<RenderComments
-							comments={props.comments}
-							addComment={props.addComment}
-							dishId={props.dish.id}
-						/>
-					</div>
+					<Loading />
 				</div>
 			</div>
-		</React.Fragment>
-	);
+		);
+	} else if (props.errMess) {
+		return (
+			<div className='container'>
+				<div className='row'>
+					<h4>{props.errMess}</h4>
+				</div>
+			</div>
+		);
+	} else if (props.dish != null)
+		return (
+			<React.Fragment>
+				<div className='container'>
+					<div className='row'>
+						<Breadcrumb>
+							{/* <BreadcrumbItem>
+									<Link to='/home'>Home</Link>
+								</BreadcrumbItem> */}
+							<BreadcrumbItem>
+								<Link to='/menu'>Menu</Link>
+							</BreadcrumbItem>
+							<BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+						</Breadcrumb>
+						<div className='col-12'>
+							<h3>{props.dish.name}</h3>
+							<hr />
+						</div>
+					</div>
+					<div className='row'>
+						<div className='col-lg-5 col-sm-12 m-1'>
+							<RenderDish dish={props.dish} />
+						</div>
+						<div className='col-lg-5 col-md-5 col-sm-12 m-1'>
+							<h4>Comments </h4>
+							<RenderComments
+								comments={props.comments}
+								addComment={props.addComment}
+								dishId={props.dish.id}
+							/>
+						</div>
+					</div>
+				</div>
+			</React.Fragment>
+		);
 };
 
 const maxLength = (len) => (val) => !val || val.length <= len;
