f37a3b5dc98407dbad5badd7fa1c2ac2aba7740c
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-02-02 07:48:14 +0300

    added reducer functionality for action types

--- React/confusion/src/redux/dishes.js
+++ React/confusion/src/redux/dishes.js
@@ -1,7 +1,31 @@
-import { DISHES } from '../shared/dishes';
+// --> DISHES REDUCER !!!
+import * as ActionTypes from './ActionTypes';
 
-export const Dishes = (state = DISHES, action) => {
+export const Dishes = (
+	state = {
+		isLoading: true,
+		errMess: null,
+		dishes: [],
+	},
+	action
+) => {
 	switch (action.type) {
+		case ActionTypes.ADD_DISHES:
+			return {
+				...state,
+				isLoading: false,
+				errMess: null,
+				dishes: action.payload,
+			};
+		case ActionTypes.DISHES_LOADING:
+			return { ...state, isLoading: true, errMess: null, dishes: [] };
+		case ActionTypes.DISHES_FAILDED:
+			return {
+				...state,
+				isLoading: false,
+				errMess: action.payload,
+			};
+
 		default:
 			return state;
 	}
