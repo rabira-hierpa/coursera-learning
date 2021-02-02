d0c5cea151714bdd2b63a0ba3f55184338efa144
Rabra Hierpa <rabira.hierpa@gmail.com>
2021-02-02 07:51:06 +0300

    mapped dispatch to props

--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -8,7 +8,7 @@ import DishDetail from './DishdetailComponent';
 import AboutUs from './AboutusComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addComment } from '../redux/ActionCreateros';
+import { addComment, fetchDishes } from '../redux/ActionCreateros';
 
 // - When retriving state from the store
 // - use mapStateToProps
@@ -28,14 +28,23 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => ({
 	addComment: (dishId, raiting, author, comment) =>
 		dispatch(addComment(dishId, raiting, author, comment)),
+	fetchDishes: () => {
+		dispatch(fetchDishes());
+	},
 });
 
 class Main extends Component {
+	componentDidMount() {
+		this.props.fetchDishes();
+	}
+
 	render() {
 		const HomePage = () => {
 			return (
 				<Home
-					dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+					dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+					dishesLoading={this.props.dishes.isLoading}
+					dishesErrMess={this.props.dishes.errMess}
 					promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
 					leader={this.props.leaders.filter((leader) => leader.featured)[0]}
 				/>
@@ -46,10 +55,12 @@ class Main extends Component {
 			return (
 				<DishDetail
 					dish={
-						this.props.dishes.filter(
+						this.props.dishes.dishes.filter(
 							(dish) => dish.id === parseInt(match.params.dishId, 10)
 						)[0]
 					}
+					isLoading={this.props.dishes.isLoading}
+					errMess={this.props.dishes.errmess}
 					comments={this.props.comments.filter(
 						(comment) => comment.dishId === parseInt(match.params.dishId, 10)
 					)}
